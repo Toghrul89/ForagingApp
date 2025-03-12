b3ad454a38ace0fbdf0ed0f1284c154e069d1016
Toghrul89 <tjaffarov@gmail.com>
2025-03-12 16:21:25 -0700

    newc

---
+++ backend/src/main/java/com/example/foragingapp/ForagingAppApplication.java
@@ -0,0 +1,11 @@
+package backend.src.main.java.com.example.foragingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForagingAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ForagingAppApplication.class, args);
+    }
+}
