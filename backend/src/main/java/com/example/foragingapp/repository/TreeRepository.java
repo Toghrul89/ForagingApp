141572829958235e1f22d9a27e8908ba18dbaed2
Toghrul89 <tjaffarov@gmail.com>
2025-07-30 09:29:25 -0700

    delete some files

--- backend/src/main/java/com/example/foragingapp/repository/TreeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.foragingapp.repository;
-
-import com.example.foragingapp.model.Tree;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface TreeRepository extends JpaRepository<Tree, UUID> {
-}
