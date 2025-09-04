302b4ce213e2fee039a5716d7f583b230dd86727
Toghrul89 <tjaffarov@gmail.com>
2025-09-04 12:42:59 -0700

    versions fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,5 @@
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.1")
-        classpath(kotlin("gradle-plugin", version = "1.9.10"))
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven { url = uri("https://maplibre.org/android/sdk/maven") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://maplibre.org/android/sdk/maven") }
+    }
+}
 rootProject.name = "ForagingApp"
 include(":app")
